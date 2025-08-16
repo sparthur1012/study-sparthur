08a8b2bd8625535621d2e73bf5f39b55a8ecc0cc
Sparthur <sparthur1012@gmail.com>
2025-08-16 14:57:54 -0300

    add prog1

--- prog1/hallo.c
+++ prog1/hallo.c
@@ -2,6 +2,22 @@
 
 int main(){
 
+int w;
+
+printf("digite sua idade\n");
+scanf("%d", &w);
+
+if (w<=18){
+
+printf("bem vindo: iniciante\n");
+
+}
+
+else {
+
+printf("ben vindo: marmanjo\n");
+
+}
 
 return 0;
 }
