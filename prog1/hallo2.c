39d858c2aec6a91908747cff641d6892112f7b74
sparthur1012 <sparthur1012@gmail.com>
2025-08-18 14:47:33 -0300

    aula

---
+++ prog1/hallo2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	
+        char nome[20];
+       	printf ("escreva uma palavra:\n");
+        scanf("%s", &nome);
+
+        while (i<=N){
+               
+	}
+        return 0;
+}
+~
---
+++ prog1/media.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int N;
+	int i=0;
+	printf ("escreva um numero:");
+	scanf("%d", & a);
+
+	while (i<=N){
+		
+	}
+	return 0;
+}
