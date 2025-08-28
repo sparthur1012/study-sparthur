2f2993342229225f5c240b494c165e855b36abcc
sparthur <sparthur1012@gmail.com>
2025-08-28 09:02:57 -0300

    inicio dos treino solo

---
+++ prog1/aula5/terminal.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	char *comando;
+	// meu terminal vaiser camado de my_term
+
+	while (1){
+		printf ("my_term>");
+		//scanf ("%s \n", comando);
+		//printf("O comando fo %s \n", comando);
+		exit(0);
+	}
+
+	return 0;
+
+}
---
+++ prog1/treinos/treino1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//Vamos começar com um Hallo World personalizado
+
+
+
+int (){
+
+	scan
+
+	return 0;
+}
