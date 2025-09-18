94c84d6db229d2e695648839144335b18d4c2328
sparthur1012 <sparthur1012@gmail.com>
2025-09-18 14:24:18 -0300

    (copy the code on the test program to the main program)

--- progc/terminal-c/terminalt1/terminalt1.c
+++ progc/terminal-c/terminalt1/terminalt1.c
@@ -4,15 +4,20 @@
 #include <string.h>
 #include <stdlib.h>
 
-//area para definir variaveis universais
-char comandos[100];
+//_____________________________________________________________________________________
 
+//area para definir variaveis universais
+char comand[100];
+
+//_____________________________________________________________________________________
 // criando funções/comandos
+
 // Função para limpar o buffer de entrada (corrigindo bugs de terminal)
-void limpar_buffer() {
+void clean_buffer() {
     int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
+    while ((c = getchar()) != '\n' && c != EOF);}
+    
+//_____________________________________________________________________________________
 
 //criar função para operações e definir variaveis
 void calc(){
@@ -32,12 +37,12 @@ void calc(){
      //receber primeiro número e atribuir ele ao int "num1"
     printf("primeiro número: "); 
     scanf ("%d", &num1);
-    limpar_buffer();
+    clean_buffer();
     
     //receber segundo número e atribuir ele ao int "num2"
     printf("segundo número: "); 
     scanf ("%d", &num2);
-    limpar_buffer();
+    clean_buffer();
 
     //comparar o operador com as respostas esperadas (strcmp) e realizar a operação atravéz disso
     if (strcmp(operador, "+") == 0) {
@@ -56,11 +61,14 @@ void calc(){
         resultado = (float) num1 / num2;
         printf("o resultado é %f\n", resultado);}
 
-    else {printf ("Por Favor escolha uma das 4 operações: soma(+), subtração(-), divisão(/) ou multiplicação(*)");}    }}
+    else {printf ("Por Favor escolha uma das 4 operações: soma(+), subtração(-), divisão(/) ou multiplicação(*)");}}}
+    
+//_____________________________________________________________________________________
 
-//criar uma função para a agenda
-void cadastrar_na_agenda(){
-    while (1)
+    //criar uma função para salvar os contatos em um arquivo de texto
+    void criar_contato(){
+
+        while (1)
     {
       //pedir para o usuario digitar o nome
     char nome[100];
@@ -68,25 +76,85 @@ void cadastrar_na_agenda(){
     
     printf("digite o nome do contato: ");
     scanf("%s", nome);
-    limpar_buffer();
-    //verificar se o usuário deseja sair
+    clean_buffer();
     if (strcmp(nome, "exit") == 0){break;}
-
+    
     //pedir para o usuario digitar o numero
     printf("digite o número do contato: ");
     scanf("%s", telefone);
-    limpar_buffer();
-    
+    clean_buffer();
+
+    //criar um arquivo para a agenda
+    FILE *arquivoagenda;
+        //abrir o arquivo em modo de anexação
+        arquivoagenda = fopen("arquivoagenda.txt", "a");
+        //verificar se o arquivo foi aberto corretamente
+        if (arquivoagenda == NULL){
+            //avisar o usuario em caso de erro
+            printf ("erro ao abrir a agenda");
+            return;
+        }
+        //salvar o contato no arquivo
+        fprintf(arquivoagenda, "Nome: %s Tel: %s\n", nome, telefone);
+        fclose(arquivoagenda);
+
     //mostrar ao usuario a agenda e o nome
-    printf("Contato Salvo (Nome: %s Tel: %s)\n", nome, telefone);}}
+    printf("Contato Salvo (Nome: %s Tel: %s)\n", nome, telefone);}}    
+    
+//_____________________________________________________________________________________
+
+    //criar função para ler arquivos da agenda
+    void ler_agenda() {
+
+    //acessar arquivo da agenda
+    FILE *arquivoagenda;
+    arquivoagenda = fopen ("arquivoagenda.txt", "r");
+    //verificar se o arquivo foi aberto corretamente
+        if (arquivoagenda == NULL){
+            //avisar o usuario em caso de erro
+            printf ("erro ao abrir a agenda");
+            return;}
+    //criar variavel para receber o string do contato
+    char lendo_agenda [1000];
+
+    //criar um loop para ler todos os contatos ao mesmo tempo
+    while (fgets(lendo_agenda, 100, arquivoagenda) != NULL){
+    printf ("%s", lendo_agenda);
+    }}
+
+//__________________________________________________________________________________
+
+//criar uma função para a agenda
+void agenda(){
+    //pedir ao usuario que decida se quer ler ou salvar um novo contato
+    //criar variavel para ler o comando do usuario
+    char agenda_cmd[10];
+    
+    // inserindo dentro de um loop while para que o usario so saia da agenda quanto pedir com o exit
+    
+    while (1){
+    printf ("salvar ou verificar?");
+    scanf ("%s", agenda_cmd);
+    clean_buffer();
+
+    if (strcmp(agenda_cmd, "exit") == 0){break;}
+    if (strcmp(agenda_cmd, "salvar") == 0){criar_contato();}
+    if (strcmp(agenda_cmd, "verificar") == 0){ler_agenda();}
+    }
+}
+
+//_____________________________________________________________________________________
 
 //criar prompt de comando para interagir com o usuario usando as funções
 int main (void){
 
     while (1){
            printf ("myterm>");
-           if(scanf ("%99s", comandos) != 1) {break;}
-           limpar_buffer ();
-           if (strcmp(comandos, "calc") == 0) {calc();}
-           if (strcmp(comandos, "agenda") == 0) {cadastrar_na_agenda ();}
-           if (strcmp (comandos, "exit") == 0){return 0;}}}
+           if(scanf ("%99s", comand) != 1) {break;}
+           clean_buffer ();
+           
+           if (strcmp (comand, "cmd") == 0) {printf (" calc: calculadora\n agenda: agenda\n exit: encerrar\n");}
+           if (strcmp(comand, "calc") == 0) {calc();}
+           if (strcmp(comand, "agenda") == 0) {agenda ();}
+           if (strcmp(comand, "exit") == 0){return 0;}}}
+//fim do codigo
