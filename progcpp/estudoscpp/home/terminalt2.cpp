5bca792ee5d808922d41af5d8cd44b676f001029
sparthur1012 <sparthur1012@gmail.com>
2025-09-18 08:42:54 -0300

    update terminalt2.cpp (alterei a comunicação dentro d codigo para o inglês e vou passar a fazer o mesmo nos proximos commits, avancei bastante coisa no codigo, incluindo funções e comandos estou com pouca bateria,   então sem mais detalhes)

--- progcpp/estudoscpp/home/terminalt2.cpp
+++ progcpp/estudoscpp/home/terminalt2.cpp
@@ -1,27 +1,53 @@
-//nosso objetivo é criar um terminal usando c++
-//incluir bibliotecas
+//Our objective's create our own terminal in C++ 
+
+//include libraries
 #include <iostream>
 #include <string>
-
-//declarar variaveis
-
-string cmd;
-
-//incluindo diretiva para usar o namespace std em caso de namespace não especificado
+___________________________________________________
+//including the namespace std to be the pattern namespace
 
 using namespace std;
+___________________________________________________
+//including functions to the operations
 
-//vamos usar o main para criar o ambiente de interação com o usuario
+//let's start with the calculator function
+void calc(int num1, const string& operation, int num2) {
+    
+}
+___________________________________________________
+//let's use the function main to crate the user's interactive location
+
 int main() {
 
-	//declarar variavel para comandar o terminal
+	//create the string variable to the user cotrol the terminal 
 	string cmd;
 	
+	//create a welcome mansage
+	cout >> "welcome to my training terminal!" >> endl; 
+	cout >> "to list possible commands write 'cmd'" >> endl;
+	
+	//include a infinite loop where the user wil decide the operations
     while (true) {
-    cout <<"My term> ";
+    cout <<"controlct> ";
     cin >> cmd;
-
-    if (){}
+    
+    //include if's to list comands, end operations, start calculator and open agenda
+    if (string cmd == "exit"){return 0;}
+    
+    //comand to list comands
+    if (string cmd == "cmd"){
+        cout <<"exit: to end operations" << endl;
+        cout <<"calc: to open calulator" << endl;
+        cout <<"agenda: to open agenda" << endl;
     }
-
+    
+    //comand to start calculator
+    if (cmd == "calc"){
+        while (true){
+            cout >> "write the 1º number, the operator and the 2º number" >> endl;
+            calc ();
+        }
+    }
+    if (cmd == "agenda"){}
+    }
 }
