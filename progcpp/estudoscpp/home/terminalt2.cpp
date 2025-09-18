0791949f69a1ce7c5bc6ce81b59acb542cc24409
sparthur <sparthur1012@gmail.com>
2025-09-17 22:16:31 -0300

    iniciei o terminal em c++, inclui bibliotecas e inclui as variaveis e entradas iniciais

--- progcpp/estudoscpp/home/terminalt2.cpp
+++ progcpp/estudoscpp/home/terminalt2.cpp
@@ -1,9 +1,27 @@
+//nosso objetivo é criar um terminal usando c++
+//incluir bibliotecas
 #include <iostream>
 #include <string>
 
+//declarar variaveis
+
+string cmd;
+
+//incluindo diretiva para usar o namespace std em caso de namespace não especificado
+
+using namespace std;
+
+//vamos usar o main para criar o ambiente de interação com o usuario
 int main() {
+
+	//declarar variavel para comandar o terminal
+	string cmd;
+	
     while (true) {
-    
+    cout <<"My term> ";
+    cin >> cmd;
+
+    if (){}
     }
 
 }
