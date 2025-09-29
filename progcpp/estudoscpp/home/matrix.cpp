156f2982f40c624d6bc7dc5dedf6d2425891bce9
sparthur1012 <sparthur1012@gmail.com>
2025-09-29 09:32:52 -0300

    update matrix.cpp "new code to calculate matrix.  creating variables and code structure"

---
+++ progcpp/estudoscpp/home/matrix.cpp
@@ -0,0 +1,31 @@
+//a simple code to do matrix operations
+//include libs
+#include <iostream>
+#include <string>
+#include <vector>
+
+//create universal variables
+//columns
+int vectcol1, vectcol2;
+//lines
+int vectln1, vectln2;
+int ln1[vectln1], ln2[vectln2];
+
+//function to create the 1º matrix
+void matrix (){
+  int col1[vectcol1], col2[vectcol2];
+
+}
+
+//include main function
+int (main) {
+  //get numbers from user
+  //columns e lines number of 1º matrix
+  std::cout << "columns number of 1º matrix" << std::endl;
+  std::cin >> colnum1;
+
+  std::cout << "lines number of 1º" << std::endl;
+  std::cin >> lnnum1;
+  void matrix1 ();
+  
+}
