da5812b34fbc12ea1a13f60d107a1517ae4b765d
sparthur1012 <sparthur1012@gmail.com>
2025-10-02 12:58:59 -0300

    new programfile in c to start to train vectors. vector.c: 'A simple code to order generc number sequencies in ascending order' include libs, create the variables and start to read the incertions of user

---
+++ progc/home/vetor.c
@@ -0,0 +1,17 @@
+// A simple code to order generc number sequencies in ascending order
+//include libs
+#include <stdio.h>
+#include <stdlib.h>
+
+//include main function
+int main (){
+    //create a vector to contain the numbers
+    int number[10];
+
+    //loop to incert numbers
+    for (int i = 1; i < 11; i++){
+        //Ask for que number on current position
+        printf ("please the %d° number", &i);
+        scanf  ("%d", &number[i]); 
+        }
+    }
