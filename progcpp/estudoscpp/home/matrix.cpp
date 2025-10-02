b8ef526024d5b5aa4985bca4bdaa63867d7abf0c
sparthur1012 <sparthur1012@gmail.com>
2025-10-02 10:05:45 -0300

    'a little bit progress in matrix.cpp but i will stop for a while to try that in c firt, c++ seens confuse to learn for a beginner like me

--- progcpp/estudoscpp/home/matrix.cpp
+++ progcpp/estudoscpp/home/matrix.cpp
@@ -10,23 +10,37 @@ int vectcol1, vectcol2;
 //lines
 int vectln1, vectln2;
 
-//function to create the 1º matrix
-void matrix (){
-  int col1[vectcol1], col2[vectcol2];
-  int ln1[vectln1] ln2[vectln2];
+//defining the structure of matrix
+using matrixdef = std::vector<std::vector(int)>>;
+
+//build the matrix
+matrixdef matrixbuild (int vecln, int vectcol){
+
+  matrixdef matrix (vectln, std::vector<int>(vectcol, 0));
+  }
+
 }
 
 //include main function
-int (main) {
+int main()) {
   //get numbers from user
   //columns e lines number of 1º matrix
   std::cout << "columns number of 1º matrix" << std::endl;
-  std::cin >> colnum1;
+  std::cin >> vectcol1;
 
   std::cout << "lines number of 1º" << std::endl;
-  std::cin >> lnnum1;
-  void matrix ();
+  std::cin >> vectln1;
 
+  //columns e lines number of 2º matrix
+  std::cout << "columns number of 2º matrix" << std::endl;
+  std::cin >> vectcol2;
 
+  std::cout << "lines number of 2º" << std::endl;
+  std::cin >> vectln2;
   
+  matrixdef matrix1 = matrixbuild(vectln1, vectcol1);
+  matrixdef matrix2 = matrixbuild(vectln2, vectcol2);
+
+  return 0;
+
 }
